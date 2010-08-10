0c774b16a869c38c5f085e4d24efc555a9d06978
Bas van Dijk <v.dijk.bas@gmail.com>
2010-08-10 22:23:32 +0200

    Added Data.Text.Lazy.IO.SaferFileHandles
    Ignore-this: a529e1d0af67a8784d3351dafc5db5fc
    
    darcs-hash:20100810202332-ae560-946d53c33eb238cb8e6c29325ebf9304f46cbb74.gz

---
+++ Data/Text/Lazy/IO/SaferFileHandles.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE UnicodeSyntax, NoImplicitPrelude, CPP #-}
+
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Text.Lazy.IO.SaferFileHandles
+-- Copyright   :  (c) 2010 Bas van Dijk
+-- License     :  BSD3 (see the file LICENSE)
+-- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
+--
+-- This module lifts the lazy 'Text' IO operations into the region monad.
+--
+-------------------------------------------------------------------------------
+
+module Data.Text.Lazy.IO.SaferFileHandles
+    ( hGetLine
+    , hGetContents
+
+    , hPutStr
+    , hPutStrLn
+    ) where
+
+
+-------------------------------------------------------------------------------
+-- Imports
+-------------------------------------------------------------------------------
+
+-- from transformers:
+import Control.Monad.IO.Class ( MonadIO )
+
+-- from text:
+import Data.Text.Lazy ( Text )
+
+#ifdef __HADDOCK__
+import qualified Data.Text.Lazy.IO as T ( hGetLine
+                                        , hGetContents
+
+                                        , hPutStr
+                                        , hPutStrLn
+                                        )
+#endif
+
+-- from explicit-iomodes-text:
+import qualified Data.Text.Lazy.IO.ExplicitIOModes as E ( hGetLine
+                                                        , hGetContents
+
+                                                        , hPutStr
+                                                        , hPutStrLn
+                                                        )
+-- from regions:
+import Control.Monad.Trans.Region ( ParentOf )
+
+-- from safer-file-handles:
+import System.IO.SaferFileHandles ( RegionalFileHandle
+                                  , ReadModes, WriteModes
+                                  )
+import System.IO.SaferFileHandles.Unsafe ( wrap, wrap2 )
+
+
+-------------------------------------------------------------------------------
+-- Lazy Text I/O with regional file handles
+-------------------------------------------------------------------------------
+
+-- | Wraps: @Data.Text.Lazy.IO.'T.hGetLine'@.
+hGetLine ∷ (pr `ParentOf` cr, MonadIO cr, ReadModes ioMode)
+         ⇒ RegionalFileHandle ioMode pr → cr Text
+hGetLine = wrap E.hGetLine
+
+-- | Wraps: @Data.Text.Lazy.IO.'T.hGetContents'@.
+hGetContents ∷ (pr `ParentOf` cr, MonadIO cr, ReadModes ioMode)
+             ⇒ RegionalFileHandle ioMode pr → cr Text
+hGetContents = wrap E.hGetContents
+
+
+-- | Wraps: @Data.Text.Lazy.IO.'T.hPutStr'@.
+hPutStr ∷ (pr `ParentOf` cr, MonadIO cr, WriteModes ioMode)
+        ⇒ RegionalFileHandle ioMode pr → Text → cr ()
+hPutStr = wrap2 E.hPutStr
+
+-- | Wraps: @Data.Text.Lazy.IO.'T.hPutStrLn'@.
+hPutStrLn ∷ (pr `ParentOf` cr, MonadIO cr, WriteModes ioMode)
+          ⇒ RegionalFileHandle ioMode pr → Text → cr ()
+hPutStrLn = wrap2 E.hPutStrLn
+
+
+-- The End ---------------------------------------------------------------------
